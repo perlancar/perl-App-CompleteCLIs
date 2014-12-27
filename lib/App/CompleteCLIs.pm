b4500c9db976dda8551fe7456544331059f00e31
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2014-12-27 10:24:53 +0700

    Initial commit

---
+++ lib/App/CompleteCLIs.pm
@@ -0,0 +1,22 @@
+package App::CompleteCLIs;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: CLI wrappers for complete_*() functions
+
+=head1 SYNOPSIS
+
+
+=head1 SEE ALSO
+
+L<Complete>
+
+C<Complete::*> modules
+
+=cut
